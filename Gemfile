bfb222d82fcaf6ed130f2ebd7422174c90bf4596
yuudainanoda <kashibayuudai@icloud.com>
2020-01-27 19:40:06 +0900

    新規登録　編集機能

--- Gemfile
+++ Gemfile
@@ -66,3 +66,5 @@ gem 'haml-rails'
 gem 'font-awesome-sass'
 
 gem 'devise'
+
+gem 'pry-rails'
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'messages/index'
+  # get 'messages/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-root "messages#index"
-resources :users,only: [:edit, :update]
+# root "messages#index"
+  root 'groups#index'
+  resources :users,only: [:edit, :update]
+  resources :groups, only: [:index, :new, :create, :edit, :update]
 end
