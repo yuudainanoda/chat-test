6edd12b168e1d4d1b4768f5d7327d5f52734537d
yuudainanoda <kashibayuudai@icloud.com>
2020-01-28 22:47:53 +0900

    メッセージ

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
 # root "messages#index"
   root 'groups#index'
   resources :users,only: [:edit, :update]
-  resources :groups, only: [:index, :new, :create, :edit, :update]
+  resources :groups, only: [:new, :create, :edit, :update] do
+  resources :messages, only: [:index, :create]
+  end
 end
