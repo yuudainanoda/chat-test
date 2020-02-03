860f19f3b11658cbf9a2562cf8ec91d934e60caf
yuudainanoda <kashibayuudai@icloud.com>
2020-02-03 13:23:21 +0900

    コントローラーテスト

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,6 +6,7 @@ class MessagesController < ApplicationController
   end
 
   def create
+    # binding.pry
     @message = @group.messages.new(message_params)
     if @message.save
       redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'
